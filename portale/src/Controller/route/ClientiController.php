1d22eb72799ee09ef197360795e347409edb3f82
krissperez <kriss23perez@gmail.com>
2024-06-20 14:07:59 +0200

    fixed clienti

--- portale/src/Controller/route/ClientiController.php
+++ portale/src/Controller/route/ClientiController.php
@@ -17,25 +17,17 @@ class ClientiController extends AbstractController
     public function __construct(EntityManagerInterface $em){
         $this->em = $em;
     }
-<<<<<<< HEAD
-    #[Route('/clienti', name: 'home')]
-=======
 
 
     #[Route('/clienti', name: 'show_clienti')]
->>>>>>> refs/remotes/origin/main
     public function getClienti () : Response
     {
         $clienti = $this->em->getRepository(Clienti::class)->findAll();
 
-<<<<<<< HEAD
-        return $this->render('clienti/clienti.html.twig',    ['clienti' => $clienti]);
-=======
         foreach ($clienti as $key => $value) {
             Formatter::underscoreToCamelCaseFilter($key);
         }
 
         return $this->render('clienti/clienti.html.twig', ['clienti' => $clienti]);
->>>>>>> refs/remotes/origin/main
     }
 }
