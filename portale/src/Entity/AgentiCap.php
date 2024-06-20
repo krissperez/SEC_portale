3e708b9afb2870b1de51c5964d4a5c44a294c58b
krissperez <kriss23perez@gmail.com>
2024-06-20 10:45:11 +0200

    db v2.0

---
+++ portale/src/Entity/AgentiCap.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AgentiCapRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AgentiCapRepository::class)]
+class AgentiCap
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $id_agente = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $id_cap = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdAgente(): ?int
+    {
+        return $this->id_agente;
+    }
+
+    public function setIdAgente(int $id_agente): static
+    {
+        $this->id_agente = $id_agente;
+
+        return $this;
+    }
+
+    public function getIdCap(): ?string
+    {
+        return $this->id_cap;
+    }
+
+    public function setIdCap(string $id_cap): static
+    {
+        $this->id_cap = $id_cap;
+
+        return $this;
+    }
+}
