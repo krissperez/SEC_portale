3f8ef24cf23bcc60a2668c5e846ec927aef5c569
MicheleProcacciante <81179178+MicheleProcacciante@users.noreply.github.com>
2024-06-20 15:47:15 +0200

    ok

--- portale/src/Controller/route/ClientiController.php
+++ portale/src/Controller/route/ClientiController.php
@@ -3,13 +3,10 @@
 namespace App\Controller\route;
 
 use App\Entity\Clienti;
-use App\Helper\Formatter;
-use App\Repository\ClientiRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class ClientiController extends AbstractController
 {
@@ -18,17 +15,10 @@ class ClientiController extends AbstractController
     public function __construct(EntityManagerInterface $em){
         $this->em = $em;
     }
-
-
     #[Route('/clienti', name: 'mostra_clienti')]
-    public function getClientiWithAgents (ClientiRepository $clientiRepository) : Response
-
+    public function getClienti () : Response
     {
-        $clienti = $clientiRepository->findClientsWithAgent();
-
-        foreach ($clienti as $key => $value) {
-            Formatter::underscoreToCamelCaseFilter($key);
-        }
+        $clienti = $this->em->getRepository(Clienti::class)->findAll();
 
         return $this->render('clienti/clienti.html.twig', ['clienti' => $clienti]);
     }
