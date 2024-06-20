2ecf36bc4795bb0af1087fdd18376da2a0754f6c
MicheleProcacciante <81179178+MicheleProcacciante@users.noreply.github.com>
2024-06-20 17:07:37 +0200

    modifiche alla pagina agenti

--- portale/src/Controller/route/ClientiController.php
+++ portale/src/Controller/route/ClientiController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\route;
 
 use App\Entity\Clienti;
+use App\Helper\Formatter;
+use App\Repository\ClientiRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ClientiController extends AbstractController
 {
@@ -15,10 +18,17 @@ class ClientiController extends AbstractController
     public function __construct(EntityManagerInterface $em){
         $this->em = $em;
     }
+
+
     #[Route('/clienti', name: 'mostra_clienti')]
-    public function getClienti () : Response
+    public function getClientiWithAgents (ClientiRepository $clientiRepository) : Response
+
     {
-        $clienti = $this->em->getRepository(Clienti::class)->findAll();
+        $clienti = $clientiRepository->findClientsWithAgent();
+
+        foreach ($clienti as $key => $value) {
+            Formatter::underscoreToCamelCaseFilter($key);
+        }
 
         return $this->render('clienti/clienti.html.twig', ['clienti' => $clienti]);
     }
