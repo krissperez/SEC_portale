8b8ea55f7b753610f7f1efb314770a9abdde8887
Rodrigo Montero <rodrigomontero@Computer-portatile-di-Rodrigo.local>
2024-06-20 10:20:32 +0200

    create db

--- portale/src/Controller/ApiController.php
+++ portale/src/Controller/ApiController.php
@@ -2,7 +2,23 @@
 
 namespace App\Controller;
 
-class ApiController
-{
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class ApiController extends AbstractController
+{
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+    #[Route('/', name: 'home')]
+    public function homepage() : Response
+    {
+        $cap = $this->em->getRepository(Clienti::class)->findAll();
+
+        return $this->json($cap);
+    }
 }
