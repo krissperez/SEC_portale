1b4753ba890361c7f781d5618305efa166a41f14
krissperez <kriss23perez@gmail.com>
2024-06-20 11:54:02 +0200

    progress in clienti twig

--- portale/src/Controller/route/ClientiController.php
+++ portale/src/Controller/route/ClientiController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\route;
 
 use App\Entity\Clienti;
-use App\Entity\Province;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
