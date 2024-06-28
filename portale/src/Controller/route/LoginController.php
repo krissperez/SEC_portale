159619f9024a981128740f0fb17e191cb1ad2b41
Rodrigo Montero <rodrigomontero@Computer-portatile-di-Rodrigo.local>
2024-06-28 16:56:38 +0200

    fixing redirect

--- portale/src/Controller/route/LoginController.php
+++ portale/src/Controller/route/LoginController.php
@@ -3,15 +3,30 @@
 namespace App\Controller\route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'pagina_login')]
-    public function loginPage() : Response
+    public function loginPage(Request $request) : Response
     {
-        return $this->render('login/login.html.twig');
+        $cookieSessionID = $request->cookies->get('PHPSESSID');
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $sessionId = session_id();
+
+        if (empty($cookieSessionID) || empty($sessionId) || $cookieSessionID !== $sessionId) {
+            return $this->render('login/login.html.twig');
+        }
+
+        return $this->redirectToRoute('home');
+
+
     }
 
     #[Route('/register', name: 'pagina_registrazione')]
